96dbd8b5b7a183a949fffb578187a93570bf9c43
zafchiel <bialakropla127.9@o2.pl>
2024-01-04 21:04:48 +0100

    add most searched cats

--- src/lib/components/mostSearchedSection.tsx
+++ src/lib/components/mostSearchedSection.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import rightArrow from "../assets/icons/rightArrow.svg";
 import Image from "next/image";
+import bengal from "../assets/images/bengal.jpg";
+import norvegian from "../assets/images/norvegian.jpg";
+import savannah from "../assets/images/savannah.jpg";
+import selkirk from "../assets/images/selkirk.jpg";
 
 export default function MostSearchedSection() {
   return (
@@ -8,11 +12,48 @@ export default function MostSearchedSection() {
       <h5>Most Searched Breeds</h5>
       <hr className="h-1 bg-[#4d270c] w-12 rounded-3xl mb-4" />
       <div className="flex flex-col md:flex-row justify-between">
-        <h3 className="text-3xl font-bold">66+ Breed for you to discover</h3>
+        <h3 className="text-3xl md:text-5xl font-bold">
+          66+ Breeds for you to discover
+        </h3>
 
         <Link href="/breeds" className="flex gap-2 items-center">
           SEE MORE <Image src={rightArrow} alt="Right Arrow Icon" />
         </Link>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 justify-items-center mt-8">
+        <div>
+          <Image
+            src={bengal}
+            alt="Bengal Cat"
+            className="object-cover aspect-square rounded-2xl"
+          />
+          <p className="font-semibold mt-3">Bengal</p>
+        </div>
+        <div>
+          <Image
+            src={norvegian}
+            alt="Norvegian Forest Cat"
+            className="object-cover aspect-square rounded-2xl"
+          />
+          <p className="font-semibold mt-3">Norvegian Forest Cat</p>
+        </div>
+        <div>
+          <Image
+            src={savannah}
+            alt="Savannah Cat"
+            className="object-cover aspect-square rounded-2xl"
+          />
+          <p className="font-semibold mt-3">Savannah</p>
+        </div>
+        <div>
+          <Image
+            src={selkirk}
+            alt="Selkirk Rex Cat"
+            className="object-cover aspect-square rounded-2xl"
+          />
+          <p className="font-semibold mt-3">Selkirk Rex</p>
+        </div>
       </div>
     </section>
   );
