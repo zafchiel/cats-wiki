db7d882da527b07feb0b6b31e832037b14f3c165
zafchiel <bialakropla127.9@o2.pl>
2024-01-05 18:17:31 +0100

    Update breed page layout and styling

--- src/app/[breedId]/page.tsx
+++ src/app/[breedId]/page.tsx
@@ -23,8 +23,9 @@ export default async function BreedPage({
 
   return (
     <main>
-      <div className="flex flex-col md:flex-row gap-4">
-        <div>
+      <div className="flex flex-col md:flex-row gap-4 justify-center items-center md:items-stretch">
+        <div className="relative">
+          <div className="bg-[#dec68b] w-3 md:h-36 lg:h-64 absolute right-full top-5 rounded-l-lg"></div>
           <Image
             src={breedImages[0].url}
             width={500}
