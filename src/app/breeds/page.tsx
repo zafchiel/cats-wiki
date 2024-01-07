50db35c529bb146309c049b25aa3ffb5d8f093ce
zafchiel <bialakropla127.9@o2.pl>
2024-01-07 18:24:41 +0100

    styling: breeds page list

--- src/app/breeds/page.tsx
+++ src/app/breeds/page.tsx
@@ -1,4 +1,6 @@
 import { Cat } from "@/lib/types";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function BreedsPage() {
   let breeds: Cat[] = [];
@@ -12,8 +14,25 @@ export default async function BreedsPage() {
   }
 
   return (
-    <main>
-      <pre>{JSON.stringify(breeds, null, 2)}</pre>
+    <main className="grid place-items-center">
+      <h1 className="text-6xl font-bold mb-10">All Breeds</h1>
+      <section>
+        {breeds.map((cat) => (
+          <Link
+            href={`/${cat.id}`}
+            key={cat.id}
+            className="flex gap-4 mb-8 p-4 rounded-xl hover:bg-[#e3e1dc]"
+          >
+            {/* image placeholder */}
+            <div className="w-52 h-52 bg-gray-600 rounded-xl" />
+
+            <div className="max-w-2xl">
+              <h3 className="font-semibold text-xl">{cat.name}</h3>
+              <p>{cat.description}</p>
+            </div>
+          </Link>
+        ))}
+      </section>
     </main>
   );
 }
