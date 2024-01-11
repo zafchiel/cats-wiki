bde1cdc531ab81446bbcac9ee6d455c11d556f2d
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2024-01-11 14:42:14 +0100

    modal backdrop blur

--- src/lib/components/modalContent.tsx
+++ src/lib/components/modalContent.tsx
@@ -7,7 +7,10 @@ type Props = {
 export default function ModalContent({ onClose }: Props) {
   return (
     <>
-      <div className="fixed inset-0 bg-black/50" onClick={onClose} />
+      <div
+        className="fixed inset-0 bg-black/70 backdrop-blur-sm"
+        onClick={onClose}
+      />
       <div className="fixed rounded-xl bg-[#e3e1dc] top-1/2 left-1/2 p-4 -translate-x-1/2 -translate-y-1/2 w-3/4 max-w-md">
         <div className="flex justify-end">
           <button onClick={onClose} className="px-4 py-1">
